9f49a542188c22b8a7793f61149379f9d9143b49
eagle <eagle@54e4795c-9cbc-e511-97f5-005056b96d30>
2015-10-10 20:23:55 +0000

    new better int version imported

---
+++ BergeV1.1c.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <cairo.h>
+
+#define MAXITERATE 40
+#define NORM_BITS (13)
+#define NORM_FACT ((nint_t)1 << NORM_BITS)
+
+
+typedef long nint_t;
+
+
+int iterate(nint_t real0, nint_t imag0)
+{
+   nint_t realq, imagq, real, imag;
+   int i;
+
+   real = real0;
+   imag = imag0;
+   for (i = 0; i < MAXITERATE; i++)
+   {
+     realq = (real * real) >> NORM_BITS;
+     imagq = (imag * imag) >> NORM_BITS;
+
+     if ((realq + imagq) > (nint_t) 4 * NORM_FACT)
+        break;
+
+     imag = ((real * imag) >> (NORM_BITS - 1)) + imag0;
+     real = realq - imagq + real0;
+   }
+   return i;
+}
+ 
+
+void mand_calc(int *image, nint_t realmin, nint_t imagmin, nint_t realmax, nint_t imagmax, int hres, int vres)
+{
+  nint_t deltareal, deltaimag, real0,  imag0;
+  int x, y;
+
+  deltareal = (realmax - realmin) / hres;
+  deltaimag = (imagmax - imagmin) / vres;
+
+  real0 = realmin;
+  for (x = 0; x < hres; x++)
+  {
+    imag0 = imagmax;
+    for (y = 0; y < vres; y++)
+    {
+      *(image + x + hres * y) = iterate(real0, imag0);
+      imag0 -= deltaimag;
+    }
+    real0 += deltareal;
+  }
+}
+
+
+void save_image(const int *image, int hres, int vres)
+{
+   cairo_surface_t *sfc;
+   unsigned char *pdata;
+   int x, y, stride;
+
+
+   sfc = cairo_image_surface_create(CAIRO_FORMAT_RGB24, hres, vres);
+   stride = cairo_image_surface_get_stride(sfc);
+   cairo_surface_flush(sfc);
+   pdata = cairo_image_surface_get_data(sfc);
+
+   for (y = 0; y < vres; y++, pdata += stride)
+      for (x = 0; x < hres; x++, image++)
+         *((int*) pdata + x) = *image >= MAXITERATE ? 0 : *image * 6 << 9;
+
+   cairo_surface_mark_dirty(sfc);
+   cairo_surface_write_to_png(sfc, "berge.png");
+   cairo_surface_destroy(sfc);
+}
+
+
+int main(int argc, char **argv)
+{
+   double bbox[] = {-2.0, -1.2, 0.7, 1.2};
+   int width = 800, height = 600;
+   int image[width * height];
+
+   mand_calc(image,
+         bbox[0] * NORM_FACT, bbox[1] * NORM_FACT, bbox[2] * NORM_FACT, bbox[3] * NORM_FACT,
+         width, height);
+
+   save_image(image, width, height);
+
+   return 0;
+}
+
