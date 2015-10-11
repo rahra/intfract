59c8804fde97d0e891320e0793e289a2069dfc20
Bernhard R. Fischer <bf@abenteuerland.at>
2015-10-11 20:26:37 +0000

    added comments and license to the source

--- BergeV1.1c.c
+++ BergeV1.1c.c
@@ -1,22 +1,55 @@
+/* Copyright 2015 Bernhard R. Fischer, 2048R/5C5FFD47 <bf@abenteuerland.at>
+ *
+ * IntFract is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * IntFract is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IntFract. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* This program calculates fractal images. It shows how to use integer
+ * arithmetics instead of floating point operations.
+ *
+ * I wrote this program originally at about 1990 for the Amiga 500 in C and
+ * also wrote an assembler version which contain some additional speed
+ * improvements.
+ *
+ * @author Bernhard R. Fischer
+ * @version 2015/10/11 (this version)
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <cairo.h>
 
+// maximum number of iterations of the inner loop
 #define MAXITERATE 64
 
+// Define to use double (floating point operations), otherwise integer
+// arithmetics is used.
 //#define USE_DOUBLE
 #ifdef USE_DOUBLE
 #define NORM_FACT 1
 typedef double nint_t;
 #else
-#define NORM_BITS 24
+#define NORM_BITS 13
 #define NORM_FACT ((nint_t)1 << NORM_BITS)
 typedef long nint_t;
 #endif
 
 
 #ifdef USE_DOUBLE
+/*! This function contains the iteration loop using integer arithmetics.
+ * @param real0 Real coordinate of pixel within the complex plane.
+ * @param imag0 Imaginary coordinate of the pixel.
+ * @return Returns the number of iterations to reach the break condition.
+ */
 int iterate(nint_t real0, nint_t imag0)
 {
    nint_t realq, imagq, real, imag;
@@ -38,6 +71,11 @@ int iterate(nint_t real0, nint_t imag0)
    return i;
 }
 #else
+/*! This function contains the iteration loop using integer arithmetics.
+ * @param real0 Real coordinate of pixel within the complex plane.
+ * @param imag0 Imaginary coordinate of the pixel.
+ * @return Returns the number of iterations to reach the break condition.
+ */
 int iterate(nint_t real0, nint_t imag0)
 {
    nint_t realq, imagq, real, imag;
@@ -61,6 +99,16 @@ int iterate(nint_t real0, nint_t imag0)
 #endif
 
 
+/*! This function contains the outer loop, i.e. calculate the coordinates
+ * within the complex plane for each pixel and then call iterate().
+ * @param image Pointer to image array of size hres * vres elements.
+ * @param realmin Minimun real value of image.
+ * @param imagmin Minimum imaginary value of image.
+ * @param realmax Maximum real value.
+ * @param imagmax Maximum imaginary value.
+ * @param hres Pixel width of image.
+ * @param vres Pixel height of image.
+ */
 void mand_calc(int *image, nint_t realmin, nint_t imagmin, nint_t realmax, nint_t imagmax, int hres, int vres)
 {
   nint_t deltareal, deltaimag, real0,  imag0;
@@ -83,14 +131,27 @@ void mand_calc(int *image, nint_t realmin, nint_t imagmin, nint_t realmax, nint_
 }
 
 
-int fract_color(int itcnt)
+/*! Translate iteration count into an RGB color value.
+ * @param itcnt Number of iterations.
+ * @return Returns an RGB color value. If itcnt is greater or equal to
+ * MAXITERATE, 0 is returned (which is black).
+ */
+int fract_color(unsigned int itcnt)
 {
-   //return itcnt >= MAXITERATE ? 0 : itcnt * (256 / MAXITERATE) << 17;
+   // red color set
+   return itcnt >= MAXITERATE ? 0 : itcnt * (256 / MAXITERATE) << 17;
+   // green and blue color set
    //return itcnt >= MAXITERATE ? 0 : (itcnt * (256 / MAXITERATE) << 1) | ((256 + itcnt * (256 / MAXITERATE)) << 10);
-   return itcnt >= MAXITERATE ? 0 : (itcnt * (256 / MAXITERATE) << 17) | ((256 + itcnt * (256 / MAXITERATE)) << 10);
+   // read and yellow color set
+   //return itcnt >= MAXITERATE ? 0 : (itcnt * (256 / MAXITERATE) << 17) | ((256 + itcnt * (256 / MAXITERATE)) << 10);
 }
 
 
+/*! Save raw pixel data to PNG file using libcairo.
+ * @param image Pointer to image array of size hres * vres elements.
+ * @param hres Pixel width of image.
+ * @param vres Pixel height of image.
+ */
 void cairo_save_image(const int *image, int hres, int vres)
 {
    cairo_surface_t *sfc;
@@ -117,21 +178,22 @@ void cairo_save_image(const int *image, int hres, int vres)
 int main(int argc, char **argv)
 {
    double bbox[] = {-2.0, -1.2, 0.7, 1.2};   // realmin, imagmin, realmax, imagmax
-   //double bbox[] = {-1.75, -0.06, -1.77, -0.08};   // realmin, imagmin, realmax, imagmax
    int width = 800, height = 600;            // pixel resolution
    int image[width * height];                // raw pixel data
 
+   // parse command line arguments
    if (argc >= 2 && !strcmp(argv[1], "-h"))
       printf("usage: %s [realmin imagmin realmax imagmax]\n", argv[0]), exit(0);
-
    if (argc >= 5)
       for (int i = 0; i < 4; i++)
          bbox[i] = atof(argv[i + 1]);
 
+   // call calculation of image
    mand_calc(image,
          bbox[0] * NORM_FACT, bbox[1] * NORM_FACT, bbox[2] * NORM_FACT, bbox[3] * NORM_FACT,
          width, height);
 
+   // save image to disk
    cairo_save_image(image, width, height);
 
    return 0;
