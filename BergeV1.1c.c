826ca8f225df42099bb295679ea4ae7909eb0cd3
Bernhard R. Fischer <bf@abenteuerland.at>
2015-10-11 06:05:24 +0000

    version now compileable as integer or float variant

--- BergeV1.1c.c
+++ BergeV1.1c.c
@@ -2,13 +2,40 @@
 #include <cairo.h>
 
 #define MAXITERATE 40
+
+//#define USE_DOUBLE
+#ifdef USE_DOUBLE
+#define NORM_FACT 1
+typedef double nint_t;
+#else
 #define NORM_BITS 13
 #define NORM_FACT ((nint_t)1 << NORM_BITS)
-
-
 typedef long nint_t;
+#endif
 
 
+#ifdef USE_DOUBLE
+int iterate(nint_t real0, nint_t imag0)
+{
+   nint_t realq, imagq, real, imag;
+   int i;
+
+   real = real0;
+   imag = imag0;
+   for (i = 0; i < MAXITERATE; i++)
+   {
+     realq = real * real;
+     imagq = imag * imag;
+
+     if ((realq + imagq) > (nint_t) 4)
+        break;
+
+     imag = real * imag * 2 + imag0;
+     real = realq - imagq + real0;
+   }
+   return i;
+}
+#else
 int iterate(nint_t real0, nint_t imag0)
 {
    nint_t realq, imagq, real, imag;
@@ -29,7 +56,8 @@ int iterate(nint_t real0, nint_t imag0)
    }
    return i;
 }
- 
+#endif
+
 
 void mand_calc(int *image, nint_t realmin, nint_t imagmin, nint_t realmax, nint_t imagmax, int hres, int vres)
 {
@@ -53,7 +81,7 @@ void mand_calc(int *image, nint_t realmin, nint_t imagmin, nint_t realmax, nint_
 }
 
 
-void save_image(const int *image, int hres, int vres)
+void cairo_save_image(const int *image, int hres, int vres)
 {
    cairo_surface_t *sfc;
    unsigned char *pdata;
@@ -77,15 +105,15 @@ void save_image(const int *image, int hres, int vres)
 
 int main(int argc, char **argv)
 {
-   double bbox[] = {-2.0, -1.2, 0.7, 1.2};
-   int width = 800, height = 600;
-   int image[width * height];
+   double bbox[] = {-2.0, -1.2, 0.7, 1.2};   // realmin, imagmin, realmax, imagmax
+   int width = 800, height = 600;            // pixel resolution
+   int image[width * height];                // raw pixel data
 
    mand_calc(image,
          bbox[0] * NORM_FACT, bbox[1] * NORM_FACT, bbox[2] * NORM_FACT, bbox[3] * NORM_FACT,
          width, height);
 
-   save_image(image, width, height);
+   cairo_save_image(image, width, height);
 
    return 0;
 }
